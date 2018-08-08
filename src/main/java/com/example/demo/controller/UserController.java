6f1bc19a8338be27eceee0bd2f2c9d65a25b41b6
fenglin <huaiyinnianshao28@163.com>
2018-08-08 17:33:24 +0800

    服务层进行Redis缓存处理

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -28,9 +28,6 @@ public class UserController {
    @Qualifier("userServiceImpl")
     private UserSerevice userService;
 
-//redisTemplate在注入时，无法声明泛型
-   @Autowired
-   private RedisTemplate redisTemplate;
 
    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     /**
@@ -53,21 +50,7 @@ public class UserController {
     @RequestMapping(value = "/users",method = RequestMethod.GET)
     @ResponseBody
     public User showUsers(String uid){
-        //从缓存中获取信息
-        ValueOperations<String,User> valueOperations=redisTemplate.opsForValue();
-        //判断缓存是否有记录,如果有记录就直接从缓存中获取信息
-        boolean hasKey=redisTemplate.hasKey(uid);
-        if(hasKey) {
-            User user= valueOperations.get(uid);
-            logger.debug("====================>从缓存中获取了用户id为"+uid+"的用户信息.");
-            return  user;
-        }
         User user =userService.showUsers(uid);
-        //如果缓存中不存在信息，在查询过后，就将信息插入缓存中
-        if(user!=null)  {
-            valueOperations.set(uid,user);
-            logger.debug("====================>将用户id为"+uid+"的用户信息插入缓存中.");
-        }
         return user;
     }
 
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,15 +1,20 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.controller.UserController;
 import com.example.demo.dao.*;
 import com.example.demo.pojo.SysPermission;
 import com.example.demo.pojo.SysUserRole;
 import com.example.demo.pojo.User;
 import com.example.demo.service.UserSerevice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -37,13 +42,19 @@ public class UserServiceImpl implements UserSerevice {
     @Autowired
     private SysPermissionDao permissionDao;
 
+    //redisTemplate在注入时，无需声明泛型
+    @Autowired
+    private RedisTemplate redisTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+
     @Override
     public User findUserByName(String name) {
         return userDao.findUserByUserName(name);
     }
 
     @Override
-    @Cacheable( cacheNames = "users" ,condition = "#uid!=null",key = "#uid")
+//    @Cacheable( cacheNames = "users" ,condition = "#uid!=null",key = "#uid")
     public User showUsers(String uid){
 //        Cache cache=cacheManager.getCache("users");
 //        User user=cache.get(uid,User.class);
@@ -53,7 +64,23 @@ public class UserServiceImpl implements UserSerevice {
 //            cache.put(uid,userDao.selectByPrimaryKey(uid));
 //            System.out.println("----------------->缓存中不存在数据");
 //        }
-        return userDao.selectByPrimaryKey(uid);
+
+        //从缓存中获取信息
+        ValueOperations<String,User> valueOperations=redisTemplate.opsForValue();
+        //判断缓存是否有记录,如果有记录就直接从缓存中获取信息
+        boolean hasKey=redisTemplate.hasKey(uid);
+        if(hasKey) {
+            User user= valueOperations.get(uid);
+            logger.debug("====================>从缓存中获取了用户id为"+uid+"的用户信息.");
+            return  user;
+        }
+        User user =userDao.selectByPrimaryKey(uid);
+        //如果缓存中不存在信息，在查询过后，就将信息插入缓存中
+        if(user!=null)  {
+            valueOperations.set(uid,user);
+            logger.debug("====================>将用户id为"+uid+"的用户信息插入缓存中.");
+        }
+        return user;
     }
 
     @Override
